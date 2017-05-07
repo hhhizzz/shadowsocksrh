beca400f240500cb85f72bac504cf55bc58bdb41
Qiweihuang <qiwei_huang@qq.com>
2017-05-07 10:24:22 +0800

    加入http进程管理功能

--- server.py
+++ server.py
@@ -14,11 +14,14 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-
+import json
+import socket
 import threading
 import os
 import logging.config
 import logging
+import time
+import datetime
 
 if __name__ == '__main__':
     import inspect
@@ -29,17 +32,94 @@ import db_transfer
 from shadowsocks import shell
 
 
-class MainThread(threading.Thread):
+class ChildTHread(threading.Thread):
     def __init__(self, obj):
-        super(MainThread, self).__init__()
-        self.daemon = True
-        self.obj = obj
+        super(ChildTHread, self).__init__()
+        self.obj = obj()
 
     def run(self):
         self.obj.thread_db()
 
     def stop(self):
         self.obj.thread_db_stop()
+
+
+class MainThread(threading.Thread):
+    def __init__(self):
+        super(MainThread, self).__init__()
+        self.daemon = True
+
+    def run(self):
+        thread = ChildTHread(db_transfer.DbTransfer)
+        thread.start()
+        host = 'localhost'
+        s = socket.socket()
+        port = 8089
+        s.bind((host, port))
+        s.listen(1)
+        while True:
+            c, addr = s.accept()
+            # 接收浏览器的请求, 不作处理
+            data = c.recv(1024)
+            form = data.split('\r\n')
+            entry = form[-1]  # main content of the request
+            logging.debug(entry)
+            logging.info(data)
+            data = {'cmd': 4}
+            if data['cmd'] == 0:
+                thread.stop()
+                now = datetime.datetime.utcnow()
+                content = '{"ok":1}'
+                expires = datetime.timedelta(days=1)
+                GMT_FORMAT = '%a, %d %b %Y %H:%M:%S GMT'
+                response = '''HTTP/1.1 200 OK
+                Server: "shadowsocksrh
+                Date: %s
+                Expires: %s
+                Content-Type: text/html;charset=utf8
+                Content-Length: %s
+                Connection: keep-alive
+
+                %s''' % (
+                    now.strftime(GMT_FORMAT),
+                    (now + expires).strftime(GMT_FORMAT),
+                    len(content),
+                    content
+                )
+                # 发送回应
+                c.send(response)
+                c.close()
+                s.close()
+                logging.info("the ssserver is stop by main server")
+                exit()
+            elif data['cmd'] == 4:
+                now = datetime.datetime.utcnow()
+                content = '{"ok":1}'
+                expires = datetime.timedelta(days=1)
+                GMT_FORMAT = '%a, %d %b %Y %H:%M:%S GMT'
+                response = '''HTTP/1.1 200 OK
+                Server: "shadowsocksrh
+                Date: %s
+                Expires: %s
+                Content-Type: text/html;charset=utf8
+                Content-Length: %s
+                Connection: keep-alive
+
+                %s''' % (
+                    now.strftime(GMT_FORMAT),
+                    (now + expires).strftime(GMT_FORMAT),
+                    len(content),
+                    content
+                )
+                # 发送回应
+                c.send(response)
+                c.close()
+                logging.info("the ssserver is reboot by main server")
+                thread.stop()
+                thread.join(1)
+                thread = ChildTHread(db_transfer.DbTransfer)
+                time.sleep(5.0)
+                thread.start()
 
 
 def main():
@@ -51,7 +131,7 @@ def main():
                         filename='log.txt',
                         filemode='a')
 
-    thread = MainThread(db_transfer.DbTransfer)
+    thread = MainThread()
 
     thread.start()
     try:
